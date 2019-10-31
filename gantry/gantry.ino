9cdfac86c829399cf3c52216881ea35d1a75f0a9
jcs0087 <56281491+jcs0087@users.noreply.github.com>
2019-10-31 17:58:34 -0500

    Update gantry.ino

--- gantry/gantry.ino
+++ gantry/gantry.ino
@@ -1,14 +1,23 @@
 #include <gantry.hpp>
 
 void setup() {
-  // put your setup code here, to run once:
-  gantry_init();
-  wait_for_coordinate();
-
+  int m=1;
+  
+  gantry_init();    //initialize gantry parameters (serial)
+  move_y_home();    //move y-axis to work area origin
+  
+  while(m != 0){    //wait to receive valid coordinate from Pi
+    m = wait_for_coordinate();  
+  }
+  move_cap_to_IL();  //move the capacitive sensor over the IL
+  position_needle(); //move needle to spot just behind IL
+  wait_for_error_check();  //adjust position of needle tip
+  inject_needle();
+  go_home();
+  
+  while(1);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly
-   //echo();
-   delay(1000);
+  // put your main code here, to run repeatedly:
 }
