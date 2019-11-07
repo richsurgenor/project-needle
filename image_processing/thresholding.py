52a0468a842ac89be155736606ee7c4bf4015a09
Andrea <andreaw2911@gmail.com>
2019-11-06 22:53:01 -0600

    Add support for kmeans/kmedoids
    Adds functions to thresholding.py to find kmeans centers and kmedoids.

--- image_processing/thresholding.py
+++ image_processing/thresholding.py
@@ -4,8 +4,9 @@ This script enhances the veins on a person's arm that has been illuminated
 with near-infrared light using Contrast Limited Adaptive Histogram
 Equalization (CLAHE), global thresholding, and adaptive thresholding.
 
-This script requires that opencv-python, numpy, maplotlib, time and argparse
-be installed within the Python environment this script is run in.
+This script requires that opencv-python, numpy, maplotlib, time, argparse,
+Pyclustering, and scikit-learn be installed within the Python environment 
+this script is run in.
 """
 
 import argparse
@@ -13,6 +14,12 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import time
+from sklearn.cluster import KMeans
+from pyclustering.cluster.kmedoids import kmedoids
+from pyclustering.cluster import cluster_visualizer
+
+from pyclustering.utils import read_sample
+from pyclustering.samples.definitions import FCPS_SAMPLES
 
 
 def apply_clahe(img, clip_lim, grid_size):
@@ -87,6 +94,46 @@ def apply_mask_brute_force(img, mask_in):
     return temp_img
 
 
+def find_kmeans(img, clusters):
+    """
+    Runs the kmeans algorithm on an image. Finds the desired number of kmeans
+    points, and returns them.
+    :param img: The input image
+    :param clusters: Number of centers to find
+    :return: The list of centers
+    """
+    kmeans = KMeans(n_clusters=clusters)
+    kmeans.fit(img)
+    y_kmeans = kmeans.predict(img)
+    centers = kmeans.cluster_centers_
+    return centers
+
+
+def find_kmedoids(data, init_med):
+    """
+    Runs the kmedoids algorithm on a set of data.  Finds and returns the kmedoids.
+    The number of medoids seems to be determined by the size of init_med.
+    :param data: Input data in the form of a list of points
+    :param init_med: List of initial medoids
+    :return: Lists of x and y coordinates of the medoids
+    """
+    # Create instance of k-medoids algorithm
+    kmedoids_instance = kmedoids(data, init_med)
+
+    # Run cluster analysis and obtain the medoids
+    kmedoids_instance.process()
+    clust = kmedoids_instance.get_clusters()
+    meds = kmedoids_instance.get_medoids()
+
+    # Extract the medoids in their own list
+    x = []
+    y = []
+    for j in range(0, len(meds)):
+        x.append(data[meds[j]][1])
+        y.append(data[meds[j]][0])
+    return x, y
+
+
 if __name__ == "__main__":
     # Parse the command line arguments
     parser = argparse.ArgumentParser()
@@ -140,14 +187,52 @@ if __name__ == "__main__":
 
     print("Total time elapsed", total_time, "s")
 
+    # Create a list of the coordinates of the black pixels in the image
+    # for the k-means and k-medoids algorithms
+    start = time.time()
+    pic_array = []
+    pic_array = list(zip(*np.where(masked_img == 0)))
+    end = time.time()
+    total_time += (end - start)
+    pic_array_np = np.array(pic_array)
+    print("Time to convert array: ", (end - start), "s")
+
+    # Run the K-Means algorithm to get 50 centers
+    start = time.time()
+    centers_list = find_kmeans(pic_array_np, 50)
+    end = time.time()
+    total_time += (end - start)
+    print("Time to run k-means: ", (end - start), "s")
+    # plt.imshow(clahe_img, 'gray')
+    # plt.scatter(centers[:, 0], centers[:, 1], c='red', s=50, alpha=0.5)
+
+    # Run the K-Medoids algorithm on the 50 centers returned by K-Means
+    # Set random initial medoids.  Also determines how many medoids will be found
+    start = time.time()
+    initial_medoids = [25, 25, 25, 25, 25]
+
+    clust_x, clust_y = find_kmedoids(centers_list, initial_medoids)
+
+    end = time.time()
+    total_time += (end - start)
+    print("Time for k-medoids operation: ", (end - start), "s")
+    print("Total time: ", total_time, "s")
+    # plt.scatter(clust_x, clust_y, c='red', s=50, alpha=0.5)
+
     # Plot all steps of the vein enhancement process
     images = [img_in, clahe_img, mask, adapt_mean_th, masked_img]
     titles = ["Original Image", "CLAHE", "Image Mask", "Adaptive Mean Thresholding", "Masked Image"]
     for i in range(1, 6):
-        plt.subplot(5, 1, i)
+        plt.subplot(3, 2, i)
         plt.imshow(images[i - 1], 'gray')
         plt.title(titles[i - 1])
+
+    # Plot the k-means and k-medoids centers
+    plt.subplot(3, 2, 6)
+    plt.imshow(clahe_img, 'gray')
+    plt.scatter(centers_list[:, 1], centers_list[:, 0], c='blue', s=10, alpha=0.5)
+    plt.scatter(clust_x, clust_y, c='red', s=25, alpha=0.5)
+    plt.title("K-Means (Blue) and K-Medoids (Red) Centers")
+
     plt.show()
     plt.clf()
-
-
